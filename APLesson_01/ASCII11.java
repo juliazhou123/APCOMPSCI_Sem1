69dbfb48d26fdd9c1e7a5e608affebd0bcfedd01
juliazhou123 <zhou2julia@gmail.com>
2016-09-06 08:34:21 -0700

    Updates for Lesson 1 Projects

---
+++ APLesson_01/ASCII11.java
@@ -0,0 +1,16 @@
+public class ASCII11
+{
+	public static void main(String[]args)
+	{
+		System.out.println(" |\\/| ----");
+		System.out.println("=(--)=_____ \\");
+		System.out.println("c___ (______/ \n");
+		
+		System.out.println("       .-.");
+		System.out.println("      (. .)__,')");
+		System.out.println("      / V      )");
+		System.out.println("()(   \\     \\/");
+		System.out.println("<)-`\\()`._`._ \\");
+		System.out.println("");
+	}
+}
