3ae9e1ef440a99c9f01ffa71756272bce0963817
juliazhou123 <zhou2julia@gmail.com>
2016-09-06 08:20:49 -0700

    Added Files

---
+++ APLesson_01/ASCII1.java
@@ -0,0 +1,9 @@
+public class ASCII1
+{
+	public static void main(String[]args)
+	{
+		System.out.println("|\\/| ----");
+		System.out.println("=(--)=_____ \\");
+		System.out.println("c___ (______/");
+	}
+}
