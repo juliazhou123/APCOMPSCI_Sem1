8d85395a4e0ef3fd5884914e1a19ca7ff1d17bcb
juliazhou123 <zhou2julia@gmail.com>
2016-09-08 07:50:39 -0700

    Lesson 2

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,19 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int x = 3;
+		int y = 4;
+		int product = x * y;
+		
+		System.out.println(x + " multiplied by "+ y + " is " + product);
+		
+		String name = Julia;
+		int address number = 12;
+		String address = Road;
+		String city = SanDiego;
+		int zip = 9213;
+		
+		System.out.println("name" + "\n" + address number + "\n" + "address" + "\n" + "city" +"\n" + zip);
+	}
+}
